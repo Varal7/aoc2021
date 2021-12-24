ab421c942d0df3c48e79149f9beb59d487670c22
Victor Quach <varaal@gmail.com>
2021-12-23 17:38:27 -0700

    DP version for day 23

--- day23/main.py
+++ day23/main.py
@@ -17,178 +17,110 @@ def main(filename):
     with open(filename) as f:
         lines = f.readlines()
 
+    HALLWAY = 1
     LOWER = 3
     UPPER = 2
 
-    hallways = [ (1,1), (1,2), (1,4), (1,6), (1,8), (1,10), (1,11), ]
+    #  hallways = [ (1,1), (1,2), (1,4), (1,6), (1,8), (1,10), (1,11)]
+    hallways = [1,2,4,6,8,10,11]
+    base = {"A": 3, "B": 5, "C": 7, "D": 9}
+    energy = {"A": 1, "B": 10, "C": 100, "D": 1000}
 
-    def leq(li1, li2):
-        if len(li1) != len(li2):
+    i2k = "ABCDE"
+    k2i = {o: i for (i,o) in enumerate(i2k)}
+
+    final = ("AA", "BB", "CC", "DD", "E" * 12)
+
+    def is_base_clean(state, k):
+        return state[k2i[k]] in [f"EE", f"E{k}", f"{k}{k}"]
+
+    def hallway_clear(state, j1, j2):
+        if state[-1][j2] != "E":
             return False
-        for el in li1:
-            if el not in li2:
+        j1, j2 = min(j1, j2), max(j1, j2)
+        for j in range(j1 + 1, j2):
+            if state[-1][j] != "E":
                 return False
         return True
 
-    class Node:
-        def __init__(self, A, B, C, D):
-            self.A = sorted(A)
-            self.B = sorted(B)
-            self.C = sorted(C)
-            self.D = sorted(D)
-
-            self.dict = {
-                "A": self.A,
-                "B": self.B,
-                "C": self.C,
-                "D": self.D,
-            }
-
-            self.pos2k = dict()
-
-            for k in "ABCD":
-                for pos in self.dict[k]:
-                    self.pos2k[pos] = k
-
-
-        def to_tuple(self):
-            return tuple(el for li in self.dict.values() for el in li)
-
-        def __lt__(self, other):
-            return self.to_tuple() < other.to_tuple()
-
-        def __eq__(self, other):
-            return self.to_tuple() == other.to_tuple()
-
-        def __hash__(self):
-            return self.to_tuple().__hash__()
-
-        @classmethod
-        def from_lines(cls, lines):
-            d = defaultdict(list)
-            for i in [1,2,3]:
-                for j, c in enumerate(lines[i]):
-                    if c in "ABCD":
-                        d[c].append((i,j))
-            return cls(**d)
-
-        def points(self):
-            points = dict()
-            for k in "ABCD":
-                for i in [0,1]:
-                    points[f"{k}{i}"] =  self.dict[k][i]
-
-            return points
-
-        def __str__(self):
-            grid = [["." for _ in range(13)] for _ in range(5)]
-            for k in "ABCD":
-                for pos in self.dict[k]:
-                    x, y = pos
-                    grid[x][y] = k
-            return "\n".join(["".join(line) for line in grid])
-
-        def heuristic(self):
-            cost = 0
-            for k in "ABCD":
-                for i in [0, 1]:
-                    s = self.dict[k][i]
-                    if s[1] != self.base(k):
-                        cost += self.cost(s, (UPPER, self.base(k)), k)
-            return cost
-
-        def base(self, k):
-            return {"A": 3, "B": 5, "C": 7, "D": 9}[k]
-
-        def energy(self, k):
-            return {"A": 1, "B": 10, "C": 100, "D": 1000}[k]
-
-        def is_base_clean(self, k):
-            j = self.base(k)
-            for i in [LOWER, UPPER]:
-                if (i, j) in self.pos2k and self.pos2k[(i,j)] != k:
-                    return False
-            return True
-
-        def cost(self, s, t, k):
-            return self.energy(k) * (abs(s[0] - t[0]) + abs(s[1] - t[1]))
-
-        def move(self, s, t):
-            k = self.pos2k[s]
-            d = {k: v for (k, v) in self.dict.items()}
-            d[k] = [pos for pos in self.dict[k] if pos != s] + [t]
-            return Node(**d)
-
-        def no_collides_inner(self, s, t):
-            if s in hallways:
-                i = s[0]
-                if t[1] > s[1]:
-                    for j in range(s[1] + 1, t[1] + 1):
-                        if (i, j) in self.pos2k:
-                            return False
-                else:
-                    for j in range(t[1], s[1]):
-                        if (i, j) in self.pos2k:
-                            return False
-                j = t[1]
-                for i in range(s[0], t[0]):
-                    if (i, j) in self.pos2k:
-                        return False
-                return True
-
+    def move(state, j, k, new_col, moving):
+        sh = state[-1]
+        s = []
+        sh = "".join(c if jj != j else moving for (jj, c) in enumerate(sh))
+        for i, sk in enumerate(state[:-1]):
+            if i != k2i[k]:
+                s.append(sk)
             else:
-                return self.no_collides_inner(t, s)
+                s.append(new_col)
+        s.append(sh)
+        return tuple(s)
 
-        def neighbours(self):
-            neis = []
-            for k in "ABCD":
-                for s in self.dict[k]:
-                    if s[1] == self.base(k) and self.is_base_clean(k):
-                        continue
-                    if s in hallways:
-                        if self.is_base_clean(k):
-                            if not (LOWER, self.base(k)) in self.pos2k:
-                                t = (LOWER, self.base(k))
-                                if self.no_collides_inner(s, t):
-                                    neis.append((self.cost(s,t,k), self.move(s, t)))
-                            else:
-                                t = (UPPER, self.base(k))
-                                if self.no_collides_inner(s, t):
-                                    neis.append((self.cost(s,t,k), self.move(s, t)))
-                    else:
-                        for t in hallways:
-                            if not t in self.pos2k:
-                                if self.no_collides_inner(s, t):
-                                    neis.append((self.cost(s,t,k), self.move(s, t)))
 
-            return neis
+    @lru_cache
+    def get_cost(state):
+        sh = state[-1]
 
+        if state == final:
+            return 0
+
+        candidates = []
+
+        # can we move from hallway?
+        for (j, k) in enumerate(sh):
+            if k != "E":
+                base_col = state[k2i[k]]
+                if hallway_clear(state, j, base[k]):
+                    if base_col == f"EE":
+                        new_state = move(state, j, k, f"E{k}", "E")
+                        cost = (abs(j - base[k]) + 2) * energy[k]
+                        if (c := get_cost(new_state)) is not None:
+                            return cost + c
+
+                    if base_col == f"E{k}":
+                        new_state = move(state, j, k, f"{k}{k}", "E")
+                        cost = (abs(j - base[k]) + 1) * energy[k]
+                        if (c := get_cost(new_state)) is not None:
+                            return cost + c
+
+        # move to hallway
+        for (i, sk) in enumerate(state[:-1]):
+            k = i2k[i]
+            if not is_base_clean(state, k):
+                if sk[0] == "E":
+                    # move the bottom one
+                    moving = sk[1]
+                    for j in hallways:
+                        if hallway_clear(state, base[k], j):
+                            new_state = move(state, j, k, "EE", moving)
+                            cost = (abs(j - base[k]) + 2) * energy[moving]
+                            if (c := get_cost(new_state)) is not None:
+                                candidates.append(cost + c)
+                else: # move the top one
+                    moving = sk[0]
+                    new_col = "E" + sk[1]
+                    for j in hallways:
+                        if hallway_clear(state, base[k], j):
+                            new_state = move(state, j, k, new_col, moving)
+                            cost = (abs(j - base[k]) + 1) * energy[moving]
+                            if (c := get_cost(new_state)) is not None:
+                                candidates.append(cost + c)
+
+        if len(candidates) == 0:
+            return None
+        return min(candidates)
 
     def part1():
-        with open("final.txt") as f:
-            final_lines = f.readlines()
-        final = Node.from_lines(final_lines)
+        nonlocal lines
+        lines = [line.rstrip().replace(".", "E") for line in lines]
+        sh = "E" + lines[HALLWAY][1:-1]
+        sa = lines[UPPER][base["A"]] + lines[LOWER][base["A"]]
+        sb = lines[UPPER][base["B"]] + lines[LOWER][base["B"]]
+        sc = lines[UPPER][base["C"]] + lines[LOWER][base["C"]]
+        sd = lines[UPPER][base["D"]] + lines[LOWER][base["D"]]
+        init_state = (sa, sb, sc, sd, sh)
+        print(init_state)
+        return get_cost(init_state)
 
-        node = Node.from_lines(lines)
-
-        for c, nei in node.neighbours():
-            print(c)
-            print(nei)
-
-        q = [(0, 0, node)]
-        visited = set()
-
-        while len(q) > 0:
-            _, cost, node = heappop(q)
-            if node == final:
-                return cost
-            visited.add(node)
-            for (d, nei) in node.neighbours():
-                if nei not in visited:
-                    heappush(q, (cost + d + nei.heuristic(), cost + d, nei))
-
-
-        return 0
 
     cprint(part1())
     #  cprint(part2())
